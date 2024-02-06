0d5985cb0b97b52867f0a139726256e1efba8359
wonhopark89-dev <wonhopark89.dev@gmail.com>
2024-02-06 15:18:28 +0900

    Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,21 @@
-
-
 // import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-// import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data'; 
+// import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
